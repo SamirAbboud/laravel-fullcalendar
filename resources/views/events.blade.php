8b8f0c8c68371ef4bbdc746da1b5bad4a9c52fdc
Samir Abboud <es_abboud@esi.dz>
2020-04-28 07:52:08 +0200

    test laravel-fullcalendar

---
+++ resources/views/events.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.app')
+
+@section('css')
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="panel panel-primary">
+            <div class="panel-heading">Event Calendar in Laravel 5 Using Laravel-fullcalendar</div>
+            <div class="panel-body">
+                {!! Form::open(array('route' => 'events.add','method'=>'POST','files'=>'true')) !!}
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        @if (Session::has('success'))
+                            <div class="alert alert-success">{{ Session::get('success') }}</div>
+                        @elseif (Session::has('warnning'))
+                            <div class="alert alert-danger">{{ Session::get('warnning') }}</div>
+                        @endif
+                    </div>
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <div class="form-group">
+                            {!! Form::label('event_name','Event Name:') !!}
+                            <div class="">
+                                {!! Form::text('event_name', null, ['class' => 'form-control']) !!}
+                                {!! $errors->first('event_name', '<p class="alert alert-danger">:message</p>') !!}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="form-group">
+                            {!! Form::label('start_date','Start Date:') !!}
+                            <div class="">
+                                {!! Form::date('start_date', null, ['class' => 'form-control']) !!}
+                                {!! $errors->first('start_date', '<p class="alert alert-danger">:message</p>') !!}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="form-group">
+                            {!! Form::label('end_date','End Date:') !!}
+                            <div class="">
+                                {!! Form::date('end_date', null, ['class' => 'form-control']) !!}
+                                {!! $errors->first('end_date', '<p class="alert alert-danger">:message</p>') !!}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-1 col-sm-1 col-md-1 text-center"> &nbsp;<br/>
+                        {!! Form::submit('Add Event',['class'=>'btn btn-primary']) !!}
+                    </div>
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+        <div class="panel panel-primary">
+            <div class="panel-heading">MY Event Details</div>
+            <div class="panel-body">
+                {!! $calendar_details->calendar() !!}
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('js')
+    <!-- Scripts -->
+    <script
+        src="https://code.jquery.com/jquery-3.5.0.min.js"
+        integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ="
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+
+    {!! $calendar_details->script() !!}
+
+@endsection
+
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('events.index');
+});
+
+Route::get('events', 'EventController@index')->name('events.index');
+Route::post('events', 'EventController@addEvent')->name('events.add');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
